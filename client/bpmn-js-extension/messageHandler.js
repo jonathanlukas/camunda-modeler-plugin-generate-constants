bd9dc23c31712b83ecab562743a95e0675a0016a
Jonathan Lukas <jonathan.lukas@camunda.com>
2022-10-20 07:42:41 +0200

    fixed a bug where a messageRef was not found

--- client/bpmn-js-extension/messageHandler.js
+++ client/bpmn-js-extension/messageHandler.js
@@ -1,11 +1,20 @@
 export default function (context) {
-    if(context.element.eventDefinitions && context.element.eventDefinitions[0].messageRef){
-        const messageName = context.element.eventDefinitions[0].messageRef.name
-        console.log(`Message found: ${messageName}`);
-        if(!context.result.messages.includes(messageName)){
-        context.result.messages.push(messageName);
-        }
+  let messageName = undefined;
+  if (context.element.messageRef) {
+    messageName = context.element.messageRef.name;
+  }
+  if (
+    context.element.eventDefinitions &&
+    context.element.eventDefinitions[0].messageRef
+  ) {
+    messageName = context.element.eventDefinitions[0].messageRef.name;
+  }
+  if (messageName) {
+    console.log(`Message found: ${messageName}`);
+    if (!context.result.messages.includes(messageName)) {
+      context.result.messages.push(messageName);
     }
+  }
 }
 
 //context.element.messageRef && context.element.messageRef.name
